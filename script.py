c4945e0d3b927734daf2fab2f51df209b415519f
elvis <elvisagbey@gmail.com>
2021-02-16 07:12:35 +0000

    initial commit for maverick data processing script

---
+++ script.py
@@ -0,0 +1,145 @@
+def data_updater(current_df, previous_df):
+    #import modules
+    import pandas as pd
+    import numpy as np
+
+    # define dimensions
+    dimensions = ['Period', 'Cycle', 'Country', 'Data Source', 'City_Region', 'Channel',
+                  'CategoryName', 'SegmentName', 'Manufacturer', 'Brand', 'ItemName',
+                  'Pack_Unit_Size']
+
+    # set of countries
+    countries = set(current_df['Country']).union(previous_df['Country'])
+
+    # empty set to contain item set additions and substrations/ommissions
+    item_set_additions = set()
+    item_set_subtractions = set()
+
+    # for each country, identify new item sets and update item_set_additions
+    for country in countries:
+
+        # filter previous and current dataframe for country
+        previous_df_filtered = previous_df[previous_df['Country'] == country]
+        current_df_filtered = current_df[current_df['Country'] == country]
+
+        # perform pandas set operations to obtain items in current df but not
+        # in previous df and update set
+        item_set_additions.update(
+            current_df_filtered[current_df_filtered['ItemName'].
+                                isin(previous_df_filtered['ItemName']) == False]['ItemName'])
+
+        # perform pandas set operations to obtain items in previous df but not
+        # in current df and update set
+        item_set_subtractions.update(
+            previous_df_filtered[previous_df_filtered['ItemName'].
+                                 isin(current_df_filtered['ItemName']) == False]['ItemName'])
+
+    # empty dataframe to hold backward data
+    df_backward = pd.DataFrame()
+
+    # check for existence of new item sets
+    if len(item_set_additions) != 0:
+
+        # get dataframe of only item set additions
+        df_additions = current_df.loc[current_df['ItemName'].isin(item_set_additions)]
+
+        # replace all metric values with nan
+        df_additions[[
+            column for column in current_df.columns if column not in dimensions]] = np.nan
+        # loop over previous periods to create backward data
+        for period in previous_df['Period'].unique():
+            df_additions.loc[:,'Period'] = period
+            df_backward = pd.concat(
+                [df_backward, df_additions], axis=0, ignore_index=True)
+
+    # create empty dataframe to hold forward data
+    df_forward = pd.DataFrame()
+
+    # check for item set ommissions
+    if len(item_set_subtractions) != 0:
+
+        # query dataframe of item set ommissions
+        df_subtractions = previous_df.loc[previous_df['ItemName'].isin(
+            item_set_subtractions)]
+
+        # set metric value of item set ommissions to nan
+        df_subtractions[[
+            column for column in previous_df.columns if column not in dimensions]] = np.nan
+
+        # create forward data for item ommissions
+        for period in current_df['Period'].unique():
+            df_subtractions.loc[:,'Period'] = period
+            df_forward = pd.concat(
+                [df_forward, df_subtractions], axis=0, ignore_index=True)
+
+    return df_backward, df_forward
+
+# function for i/o
+
+
+def read_data(path, **kwargs):
+    import pandas as pd
+
+    df = pd.read_csv(path, encoding='iso-8859-1', **kwargs)
+
+    df.dropna(axis=0, how='all', inplace=True)
+
+    return df
+
+
+# factory function
+def run_data_updater():
+    import easygui
+    import time
+    import warnings
+    import os
+    import pandas as pd
+    
+    warnings.filterwarnings("ignore")
+
+    print(">> upload previous dataset")
+    time.sleep(2)
+    path_prev = easygui.fileopenbox()
+
+    print(">> uploading previous dataset ... ")
+    try:
+        previous_data = read_data(path_prev)
+    except pd.errors.ParserError:
+        print('>> wrong file extension. Restart and Upload csv')
+        os.abort()
+    except:
+        print('>> File upload failed')
+        os.abort()
+    else:
+        print(">> data uploaded successfully")
+    
+    print(">> upload current dataset")
+    time.sleep(2)
+    path_cur = easygui.fileopenbox()
+    print(">> uploading current dataset ...")
+    try:
+        current_data = read_data(path_cur)
+        _period = current_data['Period'][0]
+    except pd.errors.ParserError:
+        print('>> wrong file extension. Restart and Upload csv')
+        os.abort()
+    except:
+        print('>> File upload failed')
+        os.abort()
+    else:
+        print(">> data uploaded successfully")
+        df_backward, df_forward = data_updater(current_data, previous_data)
+        if len(df_backward) == 0 and len(df_forward) == 0:
+            print('>> No dataset generated')
+        if len(df_backward) !=0:
+            print('>> backward dataset generated')
+            df_backward.to_csv(f'./data/backward_{_period}.csv', index=False, encoding='iso-8859-1')
+        if len(df_forward) !=0:
+            print('>> forward dataset generated')
+            df_forward.to_csv(f'./data/forward_{_period}.csv', index=False, encoding='iso-8859-1')
+        time.sleep(1)
+        print('>> Done')
+    
+
+
+run_data_updater()
